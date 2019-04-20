c7f58117957aa6e928950fc52e36e2b77794a24e
Drasungor <aguscambiano@gmail.com>
2019-04-20 14:57:25 -0300

    Subo los archivos del tp

---
+++ tp1.py
@@ -0,0 +1,15 @@
+from constant import *
+
+
+
+
+#inversion>0
+def valor_actual_neto(inversion, interes, años):
+
+
+
+    return
+
+def TP1():
+
+    inversion_inicial_en_dolares=POTENCIA_TOTAL_INSTALADA*COSTO_POTENCIA_A_INSTALAR
