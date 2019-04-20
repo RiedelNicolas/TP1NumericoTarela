21ae17c909e8e1f8adb3f41f0b90295265f6a9cf
Nicolas Riedel <nariedel99@gmail.com>
2019-04-20 19:34:27 -0300

    Secante

--- banco_de_pruebas.py
+++ banco_de_pruebas.py
@@ -1,5 +1,19 @@
+from tp1.py import*
 
-print ('hola mundo')
 
-for x in range(100):
-    print (x)
+def pendiente_secante_van(izquierda, derecha):
+    dividendo = valor_actual_neto (derecha)*(derecha - izquierda)
+    divisor = valor_actual_neto(derecha) - valor_actual_neto (izquierda)
+    return = derecha - dividendo/divisor
+
+
+def secante_van (izquierda, derecha, cota_error_porcentual)
+
+    error = 100 #inicio en  un valor suficiente para que entre al while
+    while( 100*error > cota_error_porcentual )
+        actual = pendiente_secante_van (izquierda, derecha)
+        izquierda = derecha
+        derecha = actual
+        error = abs (actual - derecha)
+
+    return actual
