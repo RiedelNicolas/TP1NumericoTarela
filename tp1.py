2447fb5fb7c654a51885a526b0a322da75a657b3
Drasungor <aguscambiano@gmail.com>
2019-04-21 17:24:22 -0300

    Se corrigio secante y se imprimen sus valores

--- tp1.py
+++ tp1.py
@@ -16,7 +16,7 @@ def biseccion_van(error_porcentual):
     interes_menor=0
     interes_mayor=1
     interes_medio=(interes_mayor+interes_menor)/2
-    #raiz_actual=medio
+    interes_medio_anterior=interes_medio
     cota_de_error=(interes_mayor-interes_menor)/2
 
     while((100*cota_de_error/interes_medio)>error_porcentual):
@@ -24,10 +24,11 @@ def biseccion_van(error_porcentual):
             interes_menor=interes_medio
         if (valor_actual_neto(interes_menor)*valor_actual_neto(interes_medio)<0):
             interes_mayor=interes_medio
+        interes_medio_anterior=interes_medio
         interes_medio=(interes_mayor+interes_menor)/2
         cota_de_error=(interes_mayor-interes_menor)/2
 
-    return interes_medio
+    return interes_medio,interes_medio_anterior
 
 
 
@@ -57,22 +58,24 @@ def punto_fijo_van(error_porcentual, interes_inicial):
 def pendiente_secante_van(ultimo_interes, anteultimo_interes):
     dividendo = valor_actual_neto(ultimo_interes) - valor_actual_neto (anteultimo_interes)
     divisor = ultimo_interes - anteultimo_interes
-    return = dividendo/divisor
-
-
-aca ameo: atom://teletype/portal/ab276839-f7c6-4c92-aaae-50b20eb4491e
+    return dividendo/divisor
 
 
 def secante_van(ultimo_interes, anteultimo_interes, cota_error_porcentual):
 
-    error = 100 #inicio en  un valor suficiente para que entre al while
-    while( 100*error > cota_error_porcentual ):
-        actual = pendiente_secante_van (ultimo_interes, anteultimo_interes)
-        ultimo_interes = anteultimo_interes
-        anteultimo_interes = actual
-        error = abs (actual - anteultimo_interes)
+    siguiente_interes = ultimo_interes- valor_actual_neto(ultimo_interes)*pendiente_secante_van (ultimo_interes, anteultimo_interes)
+    error = abs (siguiente_interes - ultimo_interes)
+    anteultimo_interes = ultimo_interes
+    ultimo_interes = siguiente_interes
 
-    return actual
+    #error = 100 #inicio en  un valor suficiente para que entre al while
+    while( (100*error/siguiente_interes) > cota_error_porcentual ):
+        siguiente_interes = ultimo_interes- valor_actual_neto(ultimo_interes)*pendiente_secante_van (ultimo_interes, anteultimo_interes)
+        error = abs (siguiente_interes - ultimo_interes)
+        anteultimo_interes = ultimo_interes
+        ultimo_interes = siguiente_interes
+
+    return siguiente_interes
 
 
 
@@ -80,12 +83,26 @@ def secante_van(ultimo_interes, anteultimo_interes, cota_error_porcentual):
 
 def TP1():
     print(inversion_inicial_en_dolares*45)
-    interes_biseccion=biseccion_van(5)
+
+    #BISECCION
+
+    interes_biseccion, interes_biseccion_anterior=biseccion_van(5)
+    print("Interes biseccion anterior:",interes_biseccion_anterior)
     print("Interes biseccion:",interes_biseccion)
     print("VAN biseccion:", valor_actual_neto(interes_biseccion))
+
+    #PUNTO FIJO
 
     interes_punto_fijo=punto_fijo_van(0.1, interes_biseccion)
     print("Interes punto fijo:", interes_punto_fijo)
     print("VAN punto fijo:", valor_actual_neto(interes_punto_fijo))
     #https://youtu.be/5kC4_iEXhGc
+
+    #SECANTE
+
+    interes_secante=secante_van(interes_biseccion, interes_biseccion_anterior, 0.1)
+    print("Interes secante:", interes_secante)
+    print("VAN secante:", valor_actual_neto(interes_secante))
+
+
 TP1()
