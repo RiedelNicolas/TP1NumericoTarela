59a91568dac7b9cb526d429da5df099d1fdb0e98
Drasungor <aguscambiano@gmail.com>
2019-04-27 22:05:44 -0300

    Cambio rango de prueba de convergencia

--- banco_de_pruebas.py
+++ banco_de_pruebas.py
@@ -64,7 +64,8 @@ def probar_convergencia_secante():
     tope=int(1*(10**4))
     #inicio=int(0.06*(10**7))
     #inicio=int(0*(10**4))
-    inicio=int(0.276*(10**4))
+    #inicio=int(0.276*(10**4))
+    inicio=int(0.277*(10**4))
     for i in range(inicio,tope):
         #resultado=imprimir_interes_secante(0.00000001,i/10000)
         resultado=imprimir_interes_secante(0.00001,i/10000)
